f28837774ebf9b26e1ecf1f920582d9fbebbbd68
Danny Tran <dtran@torontofilmschool.ca>
2023-10-18 19:58:55 -0400

    New Character Movement
    - New Movement using AddMovementInput
    - New Look using AddControllerYawInput & AddControllerPitchInput
    - New placeholder Fire function tied to left mouse pressed
    - New FPSMap for testing
    - Updated Gamemode to spawn BP_FPSCharacter

---
+++ Source/VGP221Fall2023/FPSCharacter.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSCharacter.h"
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(GEngine != nullptr)
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, FString::Printf(TEXT("Spawning BP_FPSCharacter")));
+
+	UE_LOG(LogTemp, Warning, TEXT("Spawning BP_FPSCharacter"));
+}
+
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Movement
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
+
+	// Look
+	PlayerInputComponent->BindAxis("LookHorizontal", this, &AFPSCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookVertical", this, &AFPSCharacter::AddControllerPitchInput);
+
+	// Jump
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFPSCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AFPSCharacter::EndJump);
+
+	// Fire
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFPSCharacter::Fire);
+}
+
+void AFPSCharacter::MoveForward(float value)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, value);
+}
+
+void AFPSCharacter::MoveRight(float value)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, value);
+}
+
+void AFPSCharacter::StartJump()
+{
+	bPressedJump = true;
+}
+
+void AFPSCharacter::EndJump()
+{
+	bPressedJump = false;
+}
+
+void AFPSCharacter::Fire()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Pressing Fire From Character"));
+}
--- Source/VGP221Fall2023/VGP221Fall2023GameModeBase.cpp
+++ Source/VGP221Fall2023/VGP221Fall2023GameModeBase.cpp
@@ -8,6 +8,9 @@ void AVGP221Fall2023GameModeBase::StartPlay()
 	Super::StartPlay();
 
 	// Log to Screen
+	// C Assert
 	check(GEngine != nullptr)
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Hello World"));
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Starting FPS Map")));
+
+	UE_LOG(LogTemp, Warning, TEXT("Starting FPS Map"));
 }
