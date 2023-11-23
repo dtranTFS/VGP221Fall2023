a99436c055f394bf6bab574775d6b033b69a0e38
Danny Tran <dtran@torontofilmschool.ca>
2023-11-22 19:50:54 -0500

    New Placeholder Enemy Classes
    - New enemy character
    - New enemy ai controller
    - New enemy behaviour tree & black board

---
+++ Source/VGP221Fall2023/Enemy/Task_GetRandomLocation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "Task_GetRandomLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VGP221FALL2023_API UTask_GetRandomLocation : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
